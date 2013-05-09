f7823e4837a90efd9fbefe58e94a109491c02ea2
drypot <drypot@gmail.com>
2013-05-09 16:39:27 +0900

    es5-shim.js, URI.js removed. jquery.form.js added.

--- client/public/js/base.js
+++ client/public/js/base.js
@@ -77,8 +77,19 @@ init.add(function () {
 	window.$document = $(document);
 	window.$content = $('#content');
 
-	window.url = new URI(location.toString());
-	window.query = url.query(true);
+	window.url = {};
+	window.url.pathnames = window.location.pathname.slice(1).split('/');
+	window.url.query = (function () {
+		var plusPattern = /\+/g;
+		var paramPattern = /([^&=]+)=?([^&]*)/g;
+		var search = window.location.search.slice(1);
+		var query = {};
+		var match;
+		while (match = paramPattern.exec(search)) {
+			query[match[1]] = decodeURIComponent(match[2].replace(plusPattern, " "));
+		}
+		return query;
+	})();
 
 });
 
