0d32a282fa4848ada76e5bcbbbe1a008b79723e5
drypot <drypot@gmail.com>
2013-07-28 12:55:28 +0900

    working on error page.

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var init = require('../main/init');
 var config = require('../main/config');
 var express = require('../main/express');
 
-require('../main/photo-api');
-require('../main/photo-html');
-require('../main/session-api');
-require('../main/user-api');
-require('../main/user-html');
-require('../main/hello-api');
-require('../main/static-html');
-require('../main/upload-api');
-require('../main/upload-html');
-require('../main/user-profile-html');
+// require('../main/photo-api');
+// require('../main/photo-html');
+// require('../main/session-api');
+// require('../main/user-api');
+// require('../main/user-html');
+// require('../main/hello-api');
+// require('../main/static-html');
+// require('../main/upload-api');
+// require('../main/upload-html');
+// require('../main/user-profile-html');
 
 init.run(function (err) {
 	if (err) throw err;
