e31767e3936d3f27dc7b94b8e8b78cd28c6a08be
drypot <drypot@gmail.com>
2015-03-27 14:46:34 +0900

    changed module connect-multiparty to multiparty.

--- modules/main/upload-test.js
+++ modules/main/upload-test.js
@@ -21,16 +21,17 @@ describe("parsing json", function () {
   it("given handler", function () {
     exp.core.post('/api/test/upload-json', upload.handler(function (req, res, done) {
       expect(req).json;
-      res.json({files: req.files, field: req.body.field});
+      req.body.files = req.files;
+      res.json(req.body);
       done();
     }));
   });
   it("should success", function (done) {
-    local.post('/api/test/upload-json').send({'field': 'abc'}).end(function (err, res) {
+    local.post('/api/test/upload-json').send({'p1': 'abc'}).end(function (err, res) {
       expect(err).not.exist;
       expect(res.body.err).not.exist;
-      expect(res.body.files).not.exist;
-      expect(res.body.field).equal('abc');
+      expect(res.body.files).undefined;
+      expect(res.body.p1).equal('abc');
       done();
     });
   });
@@ -39,19 +40,21 @@ describe("parsing json", function () {
 describe("parsing form", function () {
   it("given handler", function () {
     exp.core.post('/api/test/upload-form', upload.handler(function (req, res, done) {
-      // RegExp 기능이 github 에는 커밋되어 있으나 npm 패키지엔 아직 적용이 안 되어 있다.
+      // RegExp 기능이 chai-http github 에는 커밋되어 있으나 npm 패키지엔 아직 적용이 안 되어 있다.
       // expect(req).header('content-type', /multipart/);
       expect(req.header('content-type')).contain('multipart');
-      res.json({files: req.files, field: req.body.field});
+      req.body.files = req.files;
+      res.json(req.body);
       done();
     }));
   });
   it("should success", function (done) {
-    local.post('/api/test/upload-form').field('field', 'abc').end(function (err, res) {
+    local.post('/api/test/upload-form').field('p1', 'abc').field('p2', '123').field('p2', '456').end(function (err, res) {
       expect(err).not.exist;
       expect(res.body.err).not.exist;
       expect(res.body.files).eql({});
-      expect(res.body.field).equal('abc');
+      expect(res.body.p1).equal('abc');
+      expect(res.body.p2).eql(['123', '456']);
       done();
     });
   });
@@ -62,18 +65,19 @@ describe("parsing one file", function () {
   var p1;
   it("given handler", function () {
     exp.core.post('/api/test/upload-one', upload.handler(function (req, res, done) {
-      p1 = req.files.files[0].path;
+      p1 = req.files.f1[0].path;
       expect(fs.existsSync(p1)).true;
-      res.json({files: req.files, field: req.body.field});
+      req.body.files = req.files;
+      res.json(req.body);
       done();
     }));
   });
   it("should success", function (done) {
-    local.post('/api/test/upload-one').field('field', 'abc').attach('files', f1).end(function (err, res) {
+    local.post('/api/test/upload-one').field('p1', 'abc').attach('f1', f1).end(function (err, res) {
       expect(err).not.exist;
       expect(res.body.err).not.exist;
-      expect(res.body.files.files[0].safeFilename).equal('upload-fixture1.txt');
-      expect(res.body.field).equal('abc');
+      expect(res.body.p1).equal('abc');
+      expect(res.body.files.f1[0].safeFilename).equal('upload-fixture1.txt');
       expect(fs.existsSync(p1)).false;
       done();
     });
@@ -86,21 +90,22 @@ describe("parsing two files", function () {
   var p1, p2;
   it("given handler", function () {
     exp.core.post('/api/test/upload-two', upload.handler(function (req, res, done) {
-      p1 = req.files.files[0].path;
-      p2 = req.files.files[1].path;
+      p1 = req.files.f1[0].path;
+      p2 = req.files.f1[1].path;
       expect(fs.existsSync(p1)).true;
       expect(fs.existsSync(p2)).true;
-      res.json({files: req.files, field: req.body.field});
+      req.body.files = req.files;
+      res.json(req.body);
       done();
     }));
   });
   it("should success", function (done) {
-    local.post('/api/test/upload-two').field('field', 'abc').attach('files', f1).attach('files', f2).end(function (err, res) {
+    local.post('/api/test/upload-two').field('p1', 'abc').attach('f1', f1).attach('f1', f2).end(function (err, res) {
       expect(err).not.exist;
       expect(res.body.err).not.exist;
-      expect(res.body.files.files[0].safeFilename).equal('upload-fixture1.txt');
-      expect(res.body.files.files[1].safeFilename).equal('upload-fixture2.txt');
-      expect(res.body.field).equal('abc');
+      expect(res.body.p1).equal('abc');
+      expect(res.body.files.f1[0].safeFilename).equal('upload-fixture1.txt');
+      expect(res.body.files.f1[1].safeFilename).equal('upload-fixture2.txt');
       setTimeout(function () {
         expect(fs.existsSync(p1)).false;
         expect(fs.existsSync(p2)).false;
@@ -115,18 +120,19 @@ describe("parsing irregular filename", function () {
   var p1;
   it("given handler", function () {
     exp.core.post('/api/test/upload-irregular', upload.handler(function (req, res, done) {
-      p1 = req.files.files[0].path;
+      p1 = req.files.f1[0].path;
       expect(fs.existsSync(p1)).true;
-      res.json({files: req.files, field: req.body.field});
+      req.body.files = req.files;
+      res.json(req.body);
       done();
     }));
   });
   it("should success", function (done) {
-    local.post('/api/test/upload-irregular').field('field', 'abc').attach('files', f1, 'file<>()[]_-=.txt.%$#@!&.txt').end(function (err, res) {
+    local.post('/api/test/upload-irregular').field('p1', 'abc').attach('f1', f1, 'file<>()[]_-=.txt.%$#@!&.txt').end(function (err, res) {
       expect(err).not.exist;
       expect(res.body.err).not.exist;
-      expect(res.body.files.files[0].safeFilename).equal('file__()[]_-=.txt.%$#@!&.txt');
-      expect(res.body.field).equal('abc');
+      expect(res.body.files.f1[0].safeFilename).equal('file__()[]_-=.txt.%$#@!&.txt');
+      expect(res.body.p1).equal('abc');
       expect(fs.existsSync(p1)).false;
       done();
     });
--- modules/main/upload.js
+++ modules/main/upload.js
@@ -4,16 +4,14 @@ var path = require('path');
 var init = require('../base/init');
 var config = require('../base/config');
 var fsp = require('../base/fs');
-var _multiparty = require('connect-multiparty');
+var multiparty = require('multiparty');
 var exp = require('../main/express');
 
 var tmpDir;
-var multiparty;
 
 init.add(function (done) {
   console.log('upload: ' + config.uploadDir);
-  multiparty = _multiparty({ uploadDir: config.uploadDir + '/tmp' });
-  tmpDir = config.uploadDir + '/tmp',
+  tmpDir = config.uploadDir + '/tmp';
   fsp.makeDirs(tmpDir, function (err) {
     if (err) return done(err);
     fsp.emptyDir(tmpDir, done);
@@ -39,50 +37,46 @@ init.add(function (done) {
 
 exports.handler = function (inner) {
   return function (req, res, done) {
+    if (req._body) return inner(req, res, done);
 
+    var form = new multiparty.Form({ uploadDir: tmpDir });
     var paths = [];
 
-    multiparty(req, res, normalizeFiles);
-    
-    function normalizeFiles(err) {
-      if (err) return done(err);
+    form.parse(req, function(err, fields, files) {
+      var key, val;
 
-      // sample req.files
+      for (key in fields) {
+        val = fields[key];
+        req.body[key] = val.length == 1 ? val[0] : val;
+      }
+
+      // sample files.f1[0]
       //
-      // { files: [ 
-      //   { 
-      //     fieldName: 'files',
-      //     originalFilename: 'f1.txt',
-      //     path: 'upload/tmp/36597-czuain.txt',
-      //     headers: [Object],
-      //     ws: [Object],
-      //     size: 6,
-      //     name: 'f1.txt',
-      //     type: 'text/plain' 
+      // { 
+      //   fieldName: 'f1',
+      //   originalFilename: 'upload-fixture1.txt',
+      //   path: 'upload/rapixel-test/tmp/EGr-pRnhF0q6hrnSSxNfJiZC.txt',
+      //   headers: { 
+      //     'content-disposition': 'form-data; name="f1"; filename="upload-fixture1.txt"',
+      //     'content-type': 'text/plain' 
       //   },
-      //   {
-      //     ...
-      //   } 
-      // ]}
+      //   size: 6 
+      // }
 
-
-      // org fname size 0 인 경우는 어떻게 할까?
-      var files = [];
-      for (var key in req.files) {
-        if (!Array.isArray(req.files[key])) {
-          req.files[key] = [req.files[key]];
-        }
-        req.files[key].forEach(function (file) {
+      req.files = {};
+      for (key in files) {
+        req.files[key] = [];
+        files[key].forEach(function (file) {
           paths.push(file.path);
-          if (file.originalFilename.trim() /* && file.size */) {
+          if (file.originalFilename.trim()) {
             file.safeFilename = fsp.safeFilename(path.basename(file.originalFilename));
-          } else {
-            file.safeFilename = path.basename(file.path);
+            req.files[key].push(file);
           }
         });
       }      
+
       inner(req, res, deleter);
-    }
+    });
 
     function deleter(err) {
       var i = 0;
