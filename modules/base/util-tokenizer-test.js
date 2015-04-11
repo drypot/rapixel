daae6e3fcefcefdf7e28c70054f918ca88329d62
drypot <drypot@gmail.com>
2015-04-12 08:29:09 +0900

    tokenzier migrated from sleek.

---
+++ modules/base/util-tokenizer-test.js
@@ -0,0 +1,50 @@
+var expect = require('../base/chai').expect;
+
+var tokenize = require('../base/util').tokenize;
+
+describe('tokenizer', function () {
+  it('can parse emtpy', function () {
+    expect(tokenize('')).length(0);
+  });
+  it('can parse space', function () {
+    expect(tokenize(' \t\n')).length(0);
+  });
+  it('can parse numbers', function () {
+    expect(tokenize('1')).length(1).include('1');
+    expect(tokenize('12')).length(1).include('12');
+    expect(tokenize('123')).length(1).include('123');
+    expect(tokenize('1 2')).length(2).include('1').include('2');
+    expect(tokenize('12 345')).length(2).include('12').include('345');
+  });
+  it('can parse latins', function () {
+    expect(tokenize('x')).length(1).include('x');
+    expect(tokenize('x abc')).length(2).include('x').include('abc');
+  });
+  it('can skip latin dupes', function () {
+    expect(tokenize('abc def abc')).length(2).include('abc').include('def');
+  });
+  it('should ignore case', function () {
+    expect(tokenize('abc AbC dEf')).length(2).include('abc').include('def');
+  });
+  it('can parse latins with numbers', function () {
+    expect(tokenize('abc123')).length(1).include('abc123');
+  });
+  it('can parse punctuations', function () {
+    expect(tokenize('abc!')).length(1).include('abc');
+    expect(tokenize('hello, world.')).length(2).include('hello').include('world');
+  });
+  it('can parse stop words', function () {
+    expect(tokenize('what a beautiful world it is!')).length(3).include('what').include('beautiful').include('world');
+  });
+  it('can parse multiple arguments', function () {
+    expect(tokenize('abc 123', 'def 123')).length(3).include('abc').include('def').include('123')
+  });
+  it('can parse hangul', function () {
+    expect(tokenize('한')).length(0);
+    expect(tokenize('한글')).length(1).include('한글');
+    expect(tokenize('한글나')).length(2).include('한글').include('글나');
+    expect(tokenize('한글나라')).length(3).include('한글').include('글나').include('나라');
+    expect(tokenize('누나하고 나하고')).length(3).include('누나').include('나하').include('하고');
+  });
+
+});
--- modules/base/util.js
+++ modules/base/util.js
@@ -79,3 +79,64 @@ utilp.makeUrl = function(url, params) {
 
   return url;
 };
+
+// tokenzier
+
+// stop words from lucene
+
+var stops = [
+  'a', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 'for', 'if', 'in', 'into', 'is', 'it', 'no', 'not',
+  'of', 'on', 'or', 's', 'such', 't', 'that', 'the', 'their', 'then', 'there', 'these', 'they', 'this',
+  'to', 'was', 'will', 'with', '', 'www'
+];
+
+var engx = /\w+/g
+var unix = /[\u0100-\uffff]+/g
+
+utilp.tokenize = function () {
+  var tokens = [];
+  var len = arguments.length;
+  for (var i = 0; i < len; i++) {
+    tokenizeEng(arguments[i]);
+    tokenizeUni(arguments[i]);
+  }
+  return Object.keys(tokens);
+
+  function tokenizeEng(source) {
+    var engs = source.match(engx);
+    if (engs) {
+      var len = engs.length;
+      for(var i = 0; i < len; i++) {
+        var word = engs[i].toLowerCase();
+        if (~stops.indexOf(word)) {
+          continue;
+        }
+        tokens[word] = true;
+      }
+    }
+  }
+
+  function tokenizeUni(source) {
+    var unis = source.match(unix);
+    if (unis) {
+      var len = unis.length;
+      for (var i = 0; i < len; i++) {
+        var word = unis[i];
+        var wordLen = word.length;
+        if (wordLen == 1) {
+          continue;
+        }
+        var prev = undefined;
+        for (var j = 0; j < wordLen; j++) {
+          var ch = word[j]
+          if (!prev) {
+            prev = ch;
+            continue;
+          }
+          tokens[prev + ch] = true;
+          prev = ch;
+        }
+      }
+    }
+  }
+};
