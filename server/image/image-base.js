7aa923250f3b66158e2acfdb2675c2c91d4cbdd7
drypot <drypot@gmail.com>
2015-07-25 03:09:34 +0900

    image-base init updated.

--- server/image/image-base.js
+++ server/image/image-base.js
@@ -35,46 +35,53 @@ imageb.getNewId = function () {
   return ++imageId;
 };
 
-// files
+/*
+  이미지 파일 관리
 
-// 원본과 버젼이 같은 디렉토리에 저장된다는 것을 전제로 작명하였다.
-// 원본과 버젼이 같은 디렉토리에 있는 것이 좋을 것 같다.
-// 같은 형태끼리 모으지 말고 관련된 것 끼리 모아 놓는다.
-// 스토리지가 부족하면 원본/버젼을 분리할 것이 아니라
-// id 영역별로 나누는 방안을 고려하면 된다.
+  원본과 버젼이 같은 디렉토리에 저장된다는 것을 전제로 작명하였다.
+  원본과 버젼이 같은 디렉토리에 있는 것이 좋을 것 같다.
+  같은 형태끼리 모으지 말고 관련된 것 끼리 모아 놓는다.
+  스토리지가 부족하면 원본/버젼을 분리할 것이 아니라
+  id 영역별로 나누는 방안을 고려하면 된다.
 
-// 원본 파일에 -org 를 붙여 놓는다.
-// DB 없이 파일명으로 검색에 편리.
+  원본 파일에 -org 를 붙여 놓는다.
+  DB 없이 파일명으로 검색이 가능.
+*/
+
+var uploadDir;
 
 init.add(function (done) {
   fsp.makeDir(config.uploadDir + '/public/images', function (err, dir) {
     if (err) return done(err);
-
-    if (config.dev) {
-      imageb.emptyDir = function (done) {
-        fsp.emptyDir(dir, done);
-      }
-    }
-
-    imageb.FilePath = function (id, format) {
-      this.id = id;
-      this.dir = dir + '/' + fsp.makeDeepPath(id, 3);
-      if (format) {
-        this.original = this.dir + '/' + id + '-org.' + format;
-      }
-    }
-
-    imageb.FilePath.prototype.getVersion = function (width) {
-      return this.dir + '/' + this.id + '-' + width + '.jpg';
-    }
-
-    imageb.getUrlBase = function (id) {
-      return config.uploadSite + '/images/' + fsp.makeDeepPath(id, 3)
-    }
-
+    uploadDir = dir;
     done();
   });
 });
+
+init.add(function (done) {
+  if (config.dev) {
+    imageb.emptyDir = function (done) {
+      fsp.emptyDir(uploadDir, done);
+    }
+  }
+  done();
+});
+
+imageb.FilePath = function (id, format) {
+  this.id = id;
+  this.dir = uploadDir + '/' + fsp.makeDeepPath(id, 3);
+  if (format) {
+    this.original = this.dir + '/' + id + '-org.' + format;
+  }
+}
+
+imageb.FilePath.prototype.getVersion = function (width) {
+  return this.dir + '/' + this.id + '-' + width + '.jpg';
+}
+
+imageb.getUrlBase = function (id) {
+  return config.uploadSite + '/images/' + fsp.makeDeepPath(id, 3)
+}
 
 imageb.identify = function (fname, done) {
   exec('identify -format "%m %w %h" ' + fname, function (err, stdout, stderr) {
