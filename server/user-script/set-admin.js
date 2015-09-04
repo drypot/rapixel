5e686354023dd6ca4d310385ee7bc4e705c3a390
drypot <drypot@gmail.com>
2015-09-04 22:21:57 +0900

    update set-admin.js.

--- server/user-script/set-admin.js
+++ server/user-script/set-admin.js
@@ -1,13 +1,22 @@
 var init = require('../base/init');
+var error = require('../base/error');
 var config = require('../base/config');
 var mongob = require('../mongo/mongo-base');
+var userb = require('../user/user-base');
 
 init.add(function (done) {
-  if (config.argv.length < 2) {
+  if (config.argv._.length < 1) {
     console.log('\nspecify email.');
     return done();
   }
-  mongob.db.collection('users').updateOne({ email: config.argv[1] }, { $set: { admin: true } }, done);
+  userb.users.updateOne({ email: config.argv._[0] }, { $set: { admin: true } }, function (err, r) {
+    if (err) return done(err);
+    if (!r.matchedCount) {
+      console.log('user not found');
+      return done();
+    }
+    done();
+  });
 });
 
 init.run(function (err) {
